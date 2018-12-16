e6490fbc528f7838bac8a250dce948c8ff7cca23
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-16 22:38:12 +0200

    Hoisting

---
+++ examples/hoisting.js
@@ -0,0 +1,14 @@
+//Hoisting
+
+//Functions (Function can be executed before declaration)
+
+printMessage("Test hoisting");
+
+function printMessage(message) {
+    console.log(message);
+}
+
+//if variable declared with var key word then it will be added to variable object of global context before execution.
+//It will be equil undefined till moment of declaration
+console.log(hello);
+var hello = "Hello";
