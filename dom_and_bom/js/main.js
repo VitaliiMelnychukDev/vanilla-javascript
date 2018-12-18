0f708cff85aae3f8a2efe137e4b5b12a818ba813
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-18 19:36:45 +0200

    Add form on page

--- dom_and_bom/js/main.js
+++ dom_and_bom/js/main.js
@@ -1,15 +1,15 @@
 const leftMenuLies = document.querySelectorAll("#left-menu ul .article-headline");
 
 leftMenuLies.forEach((value) => {
-    value.addEventListener("mouseover", (e) => {
-        e.currentTarget.parentElement.querySelector(".li-popup").classList.remove("hidden");
+    value.addEventListener("mouseover", (el) => {
+        el.currentTarget.parentElement.querySelector(".li-popup").classList.remove("hidden");
     });
 
-    value.addEventListener("mouseout", (e) => {
-        e.currentTarget.parentElement.querySelector(".li-popup").classList.add("hidden");
+    value.addEventListener("mouseout", (el) => {
+        el.currentTarget.parentElement.querySelector(".li-popup").classList.add("hidden");
     });
 
-    value.addEventListener('click', (e) => {
+    value.addEventListener("click", (e) => {
         let articleHeadline = e.currentTarget.parentElement.dataset.article;
         buildArticleElement(articles[articleHeadline]);
         document.querySelectorAll("article .detail-item .detail-name").forEach((el) => {
@@ -31,4 +31,52 @@ function buildArticleElement(article) {
 
     const articleContent = document.querySelector("article .article-content");
     articleContent.textContent = article.content;
+}
+
+const emailForm = document.forms.emailForm;
+
+emailForm.querySelectorAll(".text-input").forEach((input) => {
+    input.onfocus = function () {
+        input.classList.add("input-onfocus");
+    };
+    input.onblur = function () {
+        input.classList.remove("input-onfocus");
+    };
+
+    input.oninput = function () {
+        input.classList.remove("input-error");
+    }
+});
+
+emailForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let error = false;
+    const inputName = e.currentTarget.elements.name;
+    if (inputName.value.length < 3) {
+        inputName.classList.add("input-error");
+        error = true;
+    }
+
+    const inputQuestion = e.currentTarget.elements.question;
+    if (inputQuestion.value.length < 10) {
+        inputQuestion.classList.add("input-error");
+        error = true;
+    }
+
+    const inputEmail = e.currentTarget.elements.email;
+    if (!isStrEmail(inputEmail.value)) {
+        inputEmail.classList.add("input-error");
+        error = true;
+    }
+
+    if (!error) {
+        const formHint = emailForm.querySelector(".hint");
+        formHint.classList.remove("hidden");
+        formHint.textContent = "There is no opportunity to ask questions at the moment. Please try later!";
+    }
+});
+
+function isStrEmail(str) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(str);
 }
