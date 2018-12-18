f75b5c9290ae912f1b9fee9ddc007ce73a83ffe1
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-18 11:28:21 +0200

    Strings

---
+++ es-2015 and higher/string.js
@@ -0,0 +1,11 @@
+let [firstName, lastName] = ["Lionel", "Messi"];
+
+console.log(`Football player ${firstName} ${lastName}.`);
+
+let str = "Hello from LA";
+console.log(str.includes("from"));//true
+console.log(str.endsWith("LA"));//true
+console.log(str.startsWith("hello"));//false
+console.log("Hi".repeat(5));//HiHiHiHiHi
+
+//In es6 symbol can have 2 and 4 bytes, before - only 2 bytes
