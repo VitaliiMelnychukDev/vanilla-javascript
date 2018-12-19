3c36163b3f84d1acf02e7606c8873aacdc54293e
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-19 11:46:29 +0200

    es2016

---
+++ es-2015 and higher/es2016.js
@@ -0,0 +1,11 @@
+//Added include array method
+
+const arr = [1, 2, 4, 7, 9];
+
+console.log(arr.includes(2)); //true
+console.log(arr.includes(3)); //false
+
+//Added ** operator. It is analog of function Math.pow(2,4);
+
+console.log(Math.pow(2, 4)); //16
+console.log(2 ** 4); //16
